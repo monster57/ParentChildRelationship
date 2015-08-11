0d43f0eb396794245fd4c482dafb233f412a15d1
monster57 <surajitbarman57@yahoo.in>
2015-08-11 10:46:04 +0530

    one javascript file is created to spike on the json data which is coming from data folder

---
+++ Javascript/jsonReader.js
@@ -0,0 +1,6 @@
+var parentChildData = require("../../Data/ParentChildData.json");
+var parentFact = require("../../Data/ParentFact.json")
+
+// console.log(parentChildData);
+console.log(parentFact);
+console.log(parentFact.length)
