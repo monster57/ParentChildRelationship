32c451902487dbcee69b7d8a51ce33e4788063b1
monster57 <surajitbarman57@yahoo.in>
2015-08-13 19:20:55 +0530

    code refactored : strings replaced with stringbulder in svgoutput file

--- showChildParentRelationShip/Svg/SvgOutput.cs
+++ showChildParentRelationShip/Svg/SvgOutput.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace ParentChildRelationship.Svg
 {
@@ -30,18 +31,43 @@ namespace ParentChildRelationship.Svg
         {
             return texts.Aggregate("",
                 (current, text) =>
-                    current +
-                    ("<text x=\"" + text.PositionX + "\" y=\"" + text.PositionY +
-                     "\" font-family=\"Verdana\" font-size=\"10\">" + text.Content + "</text>\n"));
+                    current + CreateSvgText(text.PositionX, text.PositionY, text.Content));
+        }
+
+        private static string CreateSvgText(int positionX, int positionY, string content)
+        {
+            var svgTextBulder = new StringBuilder();
+            svgTextBulder.Append("<text x=\"");
+            svgTextBulder.Append(positionX);
+            svgTextBulder.Append("\" y=\"");
+            svgTextBulder.Append(positionY);
+            svgTextBulder.Append("\" font-family=\"Verdana\" font-size=\"10\">");
+            svgTextBulder.Append(content);
+            svgTextBulder.Append("</text>\n");
+
+            return svgTextBulder.ToString();
         }
 
         private static string GetLineSvg(IEnumerable<Line> lines)
         {
             return lines.Aggregate("",
-                (current, line) => current +
-                                   ("<line x1=\"" + line.PositionX + "\" y1=\"" + line.PositionY + "\" x2=\"" +
-                                    line.PositionX1 + "\" y2=\"" + line.PositionY1 +
-                                    "\" style=\"stroke:rgb(255,0,0);stroke-width:2\"/>\n"));
+                (current, line) =>
+                    current + CreateSvgLine(line.PositionX, line.PositionY, line.PositionX1, line.PositionY1));
+        }
+
+        private static string CreateSvgLine(int positionX, int positionY, int positionX1, int positionY1)
+        {
+            var svgLineBuilder = new StringBuilder();
+            svgLineBuilder.Append("<line x1=\"");
+            svgLineBuilder.Append(positionX);
+            svgLineBuilder.Append("\" y1=\"");
+            svgLineBuilder.Append(positionY);
+            svgLineBuilder.Append("\" x2=\"");
+            svgLineBuilder.Append(positionX1);
+            svgLineBuilder.Append("\" y2=\"");
+            svgLineBuilder.Append(positionY1);
+            svgLineBuilder.Append("\" style=\"stroke:rgb(255,0,0);stroke-width:2\"/>\n");
+            return svgLineBuilder.ToString();
         }
 
         private void CreateSvgComponentsList(ICollection<Line> lines, ICollection<Text> texts)
@@ -62,19 +88,19 @@ namespace ParentChildRelationship.Svg
         private static void AddSvgComponent(ICollection<Line> lines, ICollection<Text> texts, int startingXPosition,
             int startingYPosition, Anchor anchor, ICollection<Anchor> usedAnchor)
         {
-            var oldStartingXPosition = startingXPosition;
-            var oldStartingYPosition = startingYPosition;
+            var oldPositionX = startingXPosition;
+            var oldPositionY = startingYPosition;
             startingXPosition += ConfigSettings.IncreamentedXPosition;
             if (anchor.Children == null || anchor.Children.Count == ConfigSettings.NotAcceptableValue) return;
             if (!UsedParent.Contains(anchor))
-                AddLineForIndependentParent(lines , oldStartingXPosition , startingXPosition , startingYPosition);
+                AddLineForIndependentParent(lines, oldPositionX, startingXPosition, startingYPosition);
             UsedParent.Add(anchor);
             foreach (var child in anchor.Children.Where(child => !usedAnchor.Contains(child)))
             {
                 texts.Add(new Text {Content = child.Data, PositionX = startingXPosition, PositionY = startingYPosition});
                 usedAnchor.Add(child);
-                AddChildParentConnectorLine(lines, oldStartingXPosition, startingXPosition, startingYPosition);
-                AddVerticalLine(lines, oldStartingXPosition, oldStartingYPosition, startingYPosition);
+                AddChildParentConnectorLine(lines, oldPositionX, startingXPosition, startingYPosition);
+                AddVerticalLine(lines, oldPositionX, oldPositionY, startingYPosition);
                 AddSvgComponent(lines, texts, startingXPosition, startingYPosition, child, usedAnchor);
                 UsedParent.Add(child);
                 startingYPosition += ConfigSettings.IncreamentedYPosition;
@@ -82,10 +108,10 @@ namespace ParentChildRelationship.Svg
             }
         }
 
-        private static void AddLineForIndependentParent(ICollection<Line> lines, int oldStartingXPosition,
+        private static void AddLineForIndependentParent(ICollection<Line> lines, int oldPositionX,
             int startingXPosition, int startingYPosition)
         {
-            var positionX = oldStartingXPosition + 20;
+            var positionX = oldPositionX + 20;
             var positionY = startingYPosition - 5;
             var positionX1 = startingXPosition - 20;
             var positionY1 = startingYPosition - 5;
@@ -94,20 +120,20 @@ namespace ParentChildRelationship.Svg
             AddLine(lines, positionX, positionY, positionX1, positionY1);
         }
 
-        private static void AddVerticalLine(ICollection<Line> lines, int oldStartingXPosition, int oldStartingYPosition,
+        private static void AddVerticalLine(ICollection<Line> lines, int oldPositionX, int oldPositionY,
             int startingYPosition)
         {
-            var positionX = oldStartingXPosition + 30;
-            var positionY = oldStartingYPosition - 5;
-            var positionX1 = oldStartingXPosition + 30;
+            var positionX = oldPositionX + 30;
+            var positionY = oldPositionY - 5;
+            var positionX1 = oldPositionX + 30;
             var positionY1 = startingYPosition - 5;
             AddLine(lines, positionX, positionY, positionX1, positionY1);
         }
 
-        private static void AddChildParentConnectorLine(ICollection<Line> lines, int oldStartingXPosition,
+        private static void AddChildParentConnectorLine(ICollection<Line> lines, int oldPositionX,
             int startingXPosition, int startingYPosition)
         {
-            var positionX = oldStartingXPosition + 30;
+            var positionX = oldPositionX + 30;
             var positionY = startingYPosition - 5;
             var positionX1 = startingXPosition;
             var positionY1 = startingYPosition - 5;
